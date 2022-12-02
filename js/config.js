893b684c0488e40b0788326c588225d11d2293c4
Fede Herz <fede.herz@gmail.com>
2022-12-02 10:55:37 -0300

    cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_c7bZdWa1F', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '60e5ie6kksfe8qp8gakdkrq39a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
